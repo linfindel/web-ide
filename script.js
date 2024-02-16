48ae88f147190fc145b8ddce8f7c3612f8b55b26
Linfindel <linfindel@gmail.com>
2024-02-16 15:12:58 +0000

    Add basic JS support

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function update(lang) {
 
 document.getElementById("html").value = localStorage.getItem("html");
 document.getElementById("css").value = localStorage.getItem("css");
+document.getElementById("js").value = localStorage.getItem("js");
+
+window.onmessage = function(e) {
+  if (e.data != "No errors") {
+    document.getElementById("title").innerText = e.data;
+    document.getElementById("nav").className = "error";
+  }
+
+  else {
+    document.getElementById("title").innerText = "Web IDE";
+    document.getElementById("nav").className = "";
+  }
+};
